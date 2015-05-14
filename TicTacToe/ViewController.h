ad04bcae523ca74361e3ede6e3ea4a17bb906ec8
Brent Dady <brentdady@gmail.com>
2015-05-14 14:18:37 -0700

    Initial Commit

---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Brent Dady on 5/14/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Brent Dady on 5/14/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
